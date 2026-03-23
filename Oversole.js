25664f92e6bd1bc13a895e35f19e493b732de161
Publius-Fabius <179890237+Publius-Fabius@users.noreply.github.com>
2026-03-23 16:59:04 -0700

    First working prototype.

--- Oversole.js
+++ Oversole.js
@@ -1,16 +1,31 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const { execSync } = require('child_process');
 const path = require('node:path');
+const readline = require('node:readline/promises');
+const { stdin, stdout } = require('node:process');
 
 class StdinProvider {
-    fetchResponse(instruction, prompt, history) {
+    async fetchResponse(instruction, prompt, history) {
+
         console.log(
 `INSTRUCTION:
 ${instruction}
 PROMPT:
 ${prompt}
 RESPONSE:`);
-        return fs.readFileSync(Response.stdin.fd);
+    
+        const rl = readline.createInterface(stdin, stdout);
+        
+        let text = "";
+        for(;;) {
+            const line = await rl.question(">");
+            if(line.startsWith("END"))
+                break;
+            text += `${line}\n`;
+        }
+        rl.close();
+
+        return text;
     }
 }
 
@@ -18,8 +33,6 @@ class Oversole {
 
     constructor(filePath) {
         this.filePath = filePath;
-        this.model = this.loadJSON(path.resolve(filePath));
-        this.initProvider();
     }
 
     initProvider() {
@@ -28,87 +41,90 @@ class Oversole {
                 this.provider = new StdinProvider();
                 return;
             default:
-                throw new Error(`Unknown provider: ${provider}`);
+                throw new Error(`Unknown provider: ${this.model.provider}`);
         }
     }
 
-    loadJSON(filePath) {
-        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    async loadJSON(filePath) {
+        return JSON.parse(await fs.readFile(filePath, "utf8"));
     }
 
-    saveJSON(object, filePath) {
+    async saveJSON(object, filePath) {
         const output = JSON.stringify(object, null, 4);
-        fs.writeFileSync(filePath, output, 'utf8');
+        await fs.writeFile(filePath, output, 'utf8');
     }
 
-    loadText(filePath) {
-        return fs.readFileSync(filePath, 'utf8');
+    async loadText(filePath) {
+        return (await fs.readFile(filePath, 'utf8')).toString();
     }
 
-    saveText(text, filePath) {
-        fs.writeFileSync(filePath, text, 'utf8');
+    async saveText(text, filePath) {
+        await fs.writeFile(filePath, text, 'utf8');
     }
     
     getTopTask() {
-        const stack = this.model.stack;
-        if(stack.length == 0) {
+        const stack = this.model.tasks;
+        if(stack.length == 0) 
             throw new Error(`Task stack is empty`);
-        }
         return stack[stack.length - 1];
     }
 
-    prompt() {
-        const task = getTopTask();
+    async prompt() {
+        console.log("prompting...");
+
+        const task = this.getTopTask();
         const agentP = path.resolve(this.model.agents, task.agentName);
         const agentHistoryP = path.resolve(agentP, "history.json");
-        const agentHistory = this.loadJSON(agentHistoryP);
-        const agentInstructionP = path.resolve(agentP, "INSTRUCTION.md");
-        const agentInstruction = this.loadText(agentInstructionP);
-        const program = this.loadText(this.model.program);
+        const agentHistory = await this.loadJSON(agentHistoryP);
+        const agentBioP = path.resolve(agentP, "BIOGRAPHY.md");
+        const agentBio = await this.loadText(agentBioP);
+        const agentLogP = path.resolve(agentP, "log.json");
+        const agentLog = await this.loadJSON(agentLogP);
+        const program = await this.loadText(this.model.program);
         const taskGoal = task.goal;
 
         let cache = "";
-        for(let filePath of task.cache) {
-            cache += 
-`<TEXT file="${filePath}">
-${this.loadText(filePath)}
-</TEXT>`;
-        }
+        for(let filePath of task.cache) cache += 
+`<CACHED_FILE path="${filePath}">
+${await this.loadText(filePath)}
+</CACHED_FILE>
+`;
+        let log = "";
+        for(let entry of agentLog) 
+            log += `${entry}\n`;
 
         const instruction = 
-`<INSTRUCTION>
-${agentInstruction}
-</INSTRUCTION>
-<PROGRAM>
+`<AGENT_BIOGRAPHY>
+${agentBio}
+</AGENT_BIOGRAPHY>
+<PROJECT_PROGRAM>
 ${program}
-</PROGRAM>
-<PATHS>
-projectRoot=${this.filePath}
-ontology=${this.model.ontology}
+</PROJECT_PROGRAM>
+<PROJECT_PATHS>
+atlas=${this.model.atlas}
 agents=${this.model.agents}
 work=${this.model.work}
-</PATHS>
-<CACHE>
+tmp=${this.model.tmp}
+</PROJECT_PATHS>
 ${cache}
-</CACHE>
-<GOAL>
+<LOG>
+${log}
+</LOG>
+<CURRENT_GOAL>
 ${taskGoal}
-</GOAL>
-`;
+</CURRENT_GOAL>\n`;
 
         const prompt = this.model.prompt;
-        const result = this.provider.fetchResponse(
+        const result = await this.provider.fetchResponse(
             instruction,
             prompt,
             agentHistory);
-
         agentHistory.push({ prompt, result });
-        this.saveJSON(agentHistory, agentHistoryP);
-
+        await this.saveJSON(agentHistory, agentHistoryP);
         this.model.prompt = "";
         this.model.result = result;
         this.model.mode = "EVALUATE";
-        this.saveJSON(this.model, this.filePath);
+        await this.saveJSON(this.model, this.filePath);
     }
 
     /**
@@ -124,18 +140,17 @@ ${taskGoal}
      * 
      * @param {String} text 
      */
-    doCall(text) {
+    async doCall(text) {
         const nlIndex = text.indexOf("\n");
-        if(!nlIndex) {
+        if(nlIndex == -1) 
             throw new Error("CALL response had no AGENT_NAME");
-        }
         const agentName = text.slice(0, nlIndex).trim();
         const goal = text.slice(nlIndex, text.length);
         this.model.tasks.push({ agentName, goal, cache : [] });
         this.model.prompt = goal;
         this.model.result = "";
         this.model.mode = "PROMPT";
-        this.saveJSON(this.model, this.filePath);
+        await this.saveJSON(this.model, this.filePath);
     }
     
     /**
@@ -150,12 +165,12 @@ ${taskGoal}
      * 
      * @param {String} text 
      */
-    doReturn(text) {
+    async doReturn(text) {
         this.model.tasks.pop();
         this.model.prompt = text;
         this.model.result = "";
         this.model.mode = "PROMPT";
-        this.saveJSON(this.model, this.filePath);
+        await this.saveJSON(this.model, this.filePath);
     }
 
     /**
@@ -168,16 +183,32 @@ ${taskGoal}
      * 
      * @param {String} text 
      */
-    doYield(text) {
+    async doYield(text) {
         const command = text.trim();
+        const rl = readline.createInterface(stdin, stdout);
+        
+        const answer = await rl.question(
+`Oversole wants to execute a shell command:
+${command}
+Choose y/n:`);
+        rl.close();
+        
+        if(answer.trim() != "y") {
+            console.log("The shell command was refused.");
+            return;
+        }
+        
+        console.log("Executing shell command...");
+
         try {
             const output = execSync(command, { 
                 encoding: 'utf8',
-                cwd: process.cwd() });
+                cwd: process.cwd() }).toString();
             this.model.prompt = 
-                output.toString() || "(Command executed with no output)";
+                output || "(Command executed with no output)";
         } catch (error) {
-            this.model.prompt = `ERROR executing command:\n${error.message}`;
+            this.model.prompt = 
+                `ERROR executing command:\n${error.message}`;
         }
         this.model.result = "";
         this.model.mode = "PROMPT";
@@ -196,16 +227,15 @@ ${taskGoal}
      * 
      * @param {String} text 
      */
-    doCache(text) {
+    async doCache(text) {
         const lines = text.trim().split("\n");
         const task = this.getTopTask();
-        for(line of lines) {
-            task.cache.push(line);
-        }
+        for(const line of lines) 
+            task.cache.push(path.resolve(line));
         this.model.prompt = "CACHED";
         this.model.result = "";
         this.model.mode = "PROMPT";
-        this.saveJSON(this.model, this.filePath);
+        await this.saveJSON(this.model, this.filePath);
     }
 
     /**
@@ -220,51 +250,82 @@ ${taskGoal}
      * 
      * @param {String} text 
      */
-    doDecache(text) {
+    async doDecache(text) {
         const lines = text.trim().split("\n");
         const task = this.getTopTask();
-        for(line of lines) {
-            task.cache = task.cache.filter(x => x != line);
-        }
+        for(const line of lines) 
+            task.cache = task.cache.filter(x => 
+                x != path.resolve(line));
         this.model.prompt = "DECACHED";
         this.model.result = "";
         this.model.mode = "PROMPT";
-        this.saveJSON(this.model, this.filePath);
+        await this.saveJSON(this.model, this.filePath);
     }
 
-    evaluate() {
+    /**
+     * Agent should issue log response like this:
+     * 
+     *  LOG
+     *  ${LOG_CONTENT}
+     * 
+     * Example: response = "LOG\nThis is my log.\nThis is still my log."
+     * 
+     * @param {String} text 
+     */
+    async doLog(text) {
+        const agentLogP = path.resolve(
+            this.model.agents,
+            this.getTopTask().agentName,
+            "log.json");
+        const log = await this.loadJSON(agentLogP);
+        log.push(text);
+        await this.saveJSON(log, agentLogP);
+        this.model.prompt = "LOGGED";
+        this.model.result = "";
+        this.model.mode = "PROMPT";
+        await this.saveJSON(this.model, this.filePath);
+    }
+
+    async evaluate() {
+        console.log("Evaluating result...");
+
         const text = this.model.result;
-
         const nlIndex = text.indexOf("\n");
-        if(!nlIndex) {
-            throw new Error("Response didn't contain a type line");
-        }
+        if(nlIndex == -1) 
+            throw new Error("No response type found");
         const fstLine = text.slice(0, nlIndex);
-        const rest = text.slice(nlIndex, text.length);
+        const rest = text.slice(nlIndex + 1, text.length);
 
-        if(fstLine.startsWith("CALL")) {
-            return this.doCall(rest);
-        } else if(fstLine.startsWith("RETURN")) {
-            return this.doReturn(rest);
-        } else if(fstLine.startsWith("YIELD")) {
-            return this.doYield(rest);
-        } else if(fstLine.startsWith("CACHE")) {
-            return this.doCache(rest);
-        } else if(fstLine.startsWith("DECACHE")) {
-            return this.doDecache(rest);
-        } else {
+        if(fstLine.startsWith("CALL")) 
+            return await this.doCall(rest);
+        else if(fstLine.startsWith("RETURN")) 
+            return await this.doReturn(rest);
+        else if(fstLine.startsWith("YIELD")) 
+            return await this.doYield(rest);
+        else if(fstLine.startsWith("CACHE")) 
+            return await this.doCache(rest);
+        else if(fstLine.startsWith("DECACHE")) 
+            return await this.doDecache(rest);
+        else if(fstLine.startsWith("LOG"))
+            return await this.doLog(rest);
+        else
             throw new Error("Response type was invalid");
-        }
     }
 
     async update() {
+        console.log("Updating Oversole...");
+
+        this.model = await this.loadJSON(path.resolve(this.filePath));
+        this.initProvider();
+
         switch(this.model.mode) {
             case "EVALUATE": 
-                return this.evaluate();
+                return await this.evaluate();
             case "PROMPT": 
-                return this.prompt();
+                return await this.prompt();
             default: 
-                throw new Error("Bad state machine mode");
+                throw new Error(
+                    `Invalid state machine mode: ${task.model.mode}`);
         }
     }
 };
